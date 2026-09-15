5680022e3e18153c67bee9b160f612050820ab6d
Jack Shuter <42244722+Jack278@users.noreply.github.com>
2026-09-15 21:55:07 +0100

    chore: updated text for a11y tests.

--- src/TwBlazor/Components/Tabs/TwTab.razor.cs
+++ src/TwBlazor/Components/Tabs/TwTab.razor.cs
@@ -80,7 +80,7 @@ public partial class TwTab : TwBlazorComponentBase
         get
         {
             var color = colorBuilder.GetTextColor(Color);
-            return string.IsNullOrEmpty(color) ? options.Theme.Colors.TextColors.Medium.Primary : color;
+            return string.IsNullOrEmpty(color) ? $"{options.Theme.Colors.TextColors.Medium.Primary} {options.Theme.Colors.DarkTextColors.Medium.Primary}" : color;
         }
     }
 
